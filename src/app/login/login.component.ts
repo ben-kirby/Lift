7a6548637e56ad1111c0dc599a059bf14847222b
Ben Kirby <benkrby@gmail.com>
2019-01-25 11:44:08 -0800

    Users can log in, but the logged in state is limited to the login page.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [AuthenticationService]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  user;
 
-  constructor() { }
+  constructor(public authService: AuthenticationService) { 
+    this.authService.user.subscribe(user => {
+      console.log(user);
+    });
+  }
 
-  ngOnInit() {
+  login(){
+    this.authService.login();
+  }
+
+  logout(){
+    this.authService.logout();
   }
 
 }
