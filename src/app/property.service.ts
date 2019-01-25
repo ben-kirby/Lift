771b5afba6f12de54101a44b679a8b3f686e8583
Ben Kirby <benkrby@gmail.com>
2019-01-25 13:21:05 -0800

    Testing VS Code git functionality

--- src/app/property.service.ts
+++ src/app/property.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Property } from './models/property';
+import { propertyList } from './propertyList'; 
+
 
 @Injectable()
 export class PropertyService {
 
   constructor() { }
 
+  getPropertyList(){
+    return propertyList;
+  }
 }
+ 
