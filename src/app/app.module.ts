583fdeacf87aa8dac167b51ae084c5c6cbfdb345
Ben Kirby <benkrby@gmail.com>
2019-01-25 10:34:11 -0800

    Have components for About Renter Guest and Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { RenterComponent } from './renter/renter.component';
+import { GuestComponent } from './guest/guest.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    RenterComponent,
+    GuestComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,31 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { RenterComponent } from './renter/renter.component';
+import { GuestComponent } from './guest/guest.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
     {
         path: '',
         component: WelcomeComponent
+    },
+    {
+        path: "about",
+        component: AboutComponent
+    },
+    {
+        path: "renter",
+        component: RenterComponent
+    },
+    {
+        path: "guest",
+        component: GuestComponent
+    },
+    {
+        path: "login",
+        component: LoginComponent
     }
 ];
 
