d9220a1d1a6c9e0ead22e2514f7709acbce9456c
Ben Kirby <benkrby@gmail.com>
2019-02-01 09:00:39 -0800

    Interface and pilot class created

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
+export interface Roles{
+  reader: boolean;
+  author?: boolean;
+  admin?: boolean;
+  pilot?: boolean;
+  rider?: boolean;
+}
+
 export class UserService {
 
-  constructor() { }
+  email: string;
+  name: string;
+  isPilot: boolean;
+  isRider: boolean;
 
+  constructor(authData) {
+    this
+   }
+
+}
+
+export class Pilot{
+  email: string;
+  name: string;
+  role: Roles;
+
+  constructor(authData) {
+    this.email = authData.email;
+    this.name = authData.displayName;
+    this.role = {
+      reader: true,
+      pilot: true
+    }
+  }
 }
