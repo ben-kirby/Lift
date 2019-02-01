2c3096c45f54831f68f371adfcdb58a8b9810f24
Ben Kirby <benkrby@gmail.com>
2019-02-01 09:48:51 -0800

    Working on authentication still

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,25 +1,64 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase } from 'angularfire2/database'
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router'
 import * as firebase from 'firebase/app'
+import { Pilot, Passenger } from '../authentication/authentication.component';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/switchMap';
+import { switchMap } from 'rxjs/operator/switchMap';
+
 
 @Injectable()
 export class AuthenticationService {
-  user: Observable<firebase.User>;
 
-  constructor(public afAuth: AngularFireAuth) {
-    this.user = afAuth.authState;
-  }
+  user: BehaviorSubject<User> = new BehaviorSubject(null);
 
-  logout(){
-    this.afAuth.auth.signOut();
+  constructor(private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase,
+    private router: Router) {
+
+      this.afAuth.authState.switchMap(auth => {
+        if (auth){
+          return this.db.object('users/' + auth.uid);
+        }
+        else{
+          return Observable.of(null);
+        }
+      })
+      .subscribe(user => {
+        this.user.next(user)
+      })
   }
 
   login(email, password){
-    this.afAuth.auth.signInWithEmailAndPassword(email, password);
+    const provider = new firebase.auth.EmailAuthProvider()
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then(credential => {
+        this.updateUser(credential.user)
+      })
   }
 
-  signUp(email, password){
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  private updateUser(authData){
+    const userData = new userData(authData);
+    const ref = this.db.object('users/' + authData.uid)
+    ref.take(1)
+      .subscribe(user => {
+        if (!user.role) {
+          ref.update(userData);
+        }
+      });
+  }
+
+  signUp(email, password) {
     this.afAuth.auth.createUserWithEmailAndPassword(email, password);
   }
 
